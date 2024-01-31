b1dede800e9dcd0f0b37a479dd4376830ec6ebab
charuzain <charuabhizain@gmail.com>
2024-01-31 18:26:24 -0500

    routes setup

--- server/index.js
+++ server/index.js
@@ -1,6 +1,15 @@
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 8000
+const menuRouter = require('./routes/menuRouter')
+const reviewsRouter = require('./routes/reviewsRouter')
+const chefRouter = require('./routes/chefRouter')
+
+
+
+app.use('/menu', menuRouter)
+app.use('/chef', chefRouter);
+app.use('/reviews', reviewsRouter);
 
 app.listen(PORT, () => {
   console.log("App listening on port ${PORT}")
