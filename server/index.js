ca1e7d3086386c04953390f25c306897ce947ab0
charuzain <charuabhizain@gmail.com>
2024-01-31 18:28:35 -0500

    Use cors package

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const app = express()
+const cors = require('cors');
 const PORT = process.env.PORT || 8000
 const menuRouter = require('./routes/menuRouter')
 const reviewsRouter = require('./routes/reviewsRouter')
 const chefRouter = require('./routes/chefRouter')
 
-
+app.use(cors());
+app.use(express.json());
 
 app.use('/menu', menuRouter)
 app.use('/chef', chefRouter);
--- server/routes/menuRouter.js
+++ server/routes/menuRouter.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
+const path = require('path');
+router.use(express.json());
+
+
 
 router.get('/', (req, res) => {
   res.send('Menu route');
