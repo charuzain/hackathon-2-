3a6d401e1cf879f7029760b6034e3f8f290421c3
charuzain <charuabhizain@gmail.com>
2024-01-31 19:39:18 -0500

    Add JSON data and Add Get endpoint

--- server/routes/menuRouter.js
+++ server/routes/menuRouter.js
@@ -1,13 +1,33 @@
 const express = require('express');
+const app = express();
 const router = express.Router();
 const fs = require('fs');
 const path = require('path');
 router.use(express.json());
 
 
+app.use('/images', express.static('./images'));
+
+
+
+
+const getMenu = () => {
+  const filePath = path.join(__dirname, '../data/menu.json');
+  const menuData = fs.readFileSync(filePath);
+  const parsedMenuData = JSON.parse(menuData);
+
+  return parsedMenuData;
+};
 
 router.get('/', (req, res) => {
-  res.send('Menu route');
+  const parsedMenu = getMenu();
+  const menu = parsedMenu.map((dish) => ({
+    id: dish.id,
+    price: dish.price
+    
+  }));
+
+  res.json(menu);
 });
 
 module.exports = router;
