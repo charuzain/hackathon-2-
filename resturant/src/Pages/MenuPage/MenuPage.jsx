b65895432fb6d7d6a150ab376a0fc0d376d2e85d
charuzain <charuabhizain@gmail.com>
2024-02-01 10:03:39 -0500

    Add pagination

--- resturant/src/Pages/MenuPage/MenuPage.jsx
+++ resturant/src/Pages/MenuPage/MenuPage.jsx
@@ -2,13 +2,19 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import MenuList from '../../Components/MenuList/MenuList';
 import Categories from '../../Components/Categories/Categories';
+import './MenuPage.scss';
 
 function Menu() {
   const [menu, setMenu] = useState([]);
   const [currentCategory, setCurrentCategory] = useState('all');
+  const [currentPage, setCurrentPage] = useState(1);
   const allCategories = ['all', 'burgers', 'soups', 'drinks'];
 
   const url = 'http://localhost:8000';
+
+  useEffect(() => {
+    getMenuData();
+  }, []);
 
   const getMenuData = async () => {
     try {
@@ -19,21 +25,26 @@ function Menu() {
     }
   };
 
-  useEffect(() => {
-    getMenuData();
-  }, []);
-
   const filterItems = (category) => {
     setCurrentCategory(category);
+    setCurrentPage(1);
   };
+
+  const itemsPerPage = currentCategory === 'all' ? 12 : 9;
 
   const filteredMenu =
     currentCategory === 'all'
-      ? [...menu] 
+      ? [...menu]
       : menu.filter(
           (item) =>
             item.category.toLowerCase() === currentCategory.toLowerCase()
         );
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = filteredMenu.slice(indexOfFirstItem, indexOfLastItem);
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
   return (
     <main>
@@ -42,10 +53,22 @@ function Menu() {
 
       {!menu && <h1>Loading...</h1>}
       <ul>
-        {filteredMenu.map((item) => (
+        {currentItems.map((item) => (
           <MenuList key={item.id} {...item} />
         ))}
       </ul>
+      {filteredMenu.length > itemsPerPage && (
+        <div className="pagination">
+          {Array.from(
+            { length: Math.ceil(filteredMenu.length / itemsPerPage) },
+            (_, i) => (
+              <button key={i + 1} onClick={() => paginate(i + 1)}>
+                {i + 1}
+              </button>
+            )
+          )}
+        </div>
+      )}
     </main>
   );
 }
