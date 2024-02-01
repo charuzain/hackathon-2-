d29fb27b17940abf52794a28e7681d8a67b35f0c
Yashvimangroliya <yashvimangroliya@gmail.com>
2024-02-01 09:52:37 -0500

    change path

--- resturant/src/Pages/Home/Home.js
+++ resturant/src/Pages/Home/Home.js
@@ -1,9 +1,11 @@
 import "./Home.scss";
 import Hero from "../../Components/Hero/Hero";
+import Foodies from "../../Components/Foodies/Foodies";
 function Home() {
   return (
     <main>
         <Hero></Hero>
+        <Foodies></Foodies>
     </main>
   )
 }
