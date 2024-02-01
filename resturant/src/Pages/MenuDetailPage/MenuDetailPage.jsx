da6ab3ac7fa75bc9784e972b5377d2d918b827a3
charuzain <charuabhizain@gmail.com>
2024-02-01 10:49:50 -0500

    Use useEffect and axios to fetch item details from the server

--- resturant/src/Pages/MenuDetailPage/MenuDetailPage.jsx
+++ resturant/src/Pages/MenuDetailPage/MenuDetailPage.jsx
@@ -1,14 +1,37 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import './MenuDetailPage.scss';
+import { useParams } from 'react-router-dom';
 
 
 import React from 'react'
 
 const MenuDetailPage = () => {
+   const { id } = useParams();
+  const [itemDetails, setItemDetails] = useState({});
+   useEffect(() => {
+     const fetchItemDetails = async () => {
+       try {
+         const response = await axios.get(`http://localhost:8000/menu/${id}`);
+         setItemDetails(response.data);
+       } catch (error) {
+         console.error('There was an error while fetching item details from server:', error);
+       }
+     };
+
+     fetchItemDetails();
+   }, [id]);
+  const { name, price, description, image } = itemDetails;
   return (
-    <div>MenuDetailPage</div>
-  )
+     <div>
+
+      <p>Item ID: {id}</p>
+      <p>Name: {name}</p>
+  
+  
+     
+    </div>
+  );
 }
 
 export default MenuDetailPage
