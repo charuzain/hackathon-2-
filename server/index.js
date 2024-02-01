e2e1f13f644086c9de0fb7fa37afd5aecc0268d0
charuzain <charuabhizain@gmail.com>
2024-01-31 23:45:04 -0500

    Connect backend and frontend

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const menuRouter = require('./routes/menuRouter')
 const reviewsRouter = require('./routes/reviewsRouter')
 const chefRouter = require('./routes/chefRouter')
 
-// app.use('/images', express.static('images'));
+app.use('/images', express.static('./images'));
 
 app.use(cors());
 app.use(express.json());
@@ -18,5 +18,5 @@ app.use('/chef', chefRouter);
 app.use('/reviews', reviewsRouter);
 
 app.listen(PORT, () => {
-  console.log("App listening on port ${PORT}")
+  console.log(`App listening on port ${PORT} `)
 })
