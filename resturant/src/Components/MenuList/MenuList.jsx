71b723a5bc2580de4e9d4bcefdadb1ac1e26863c
charuzain <charuabhizain@gmail.com>
2024-02-01 09:26:09 -0500

    Filter menu items based on category

--- resturant/src/Components/MenuList/MenuList.jsx
+++ resturant/src/Components/MenuList/MenuList.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 const MenuList = (props) => {
   console.log(props)
   return (
-    <li>
-      <img src={`http://localhost:8000${props.image}`} alt={props.name} />
-      <div className="item-info">
+    <li className='item'>
+      <img src={`http://localhost:8000${props.image}`} alt={props.name} className='item__image' />
+      <div className="item__info">
         <header>
-          <h5>{props.title}</h5>
-          <span className="item-price">${props.price}</span>
+          <h3 className='item__name'>{props.name}</h3>
+          <span className="item__price">${props.price}</span>
         </header>
-        <p className="item-text">{props.description}</p>
+        <p className="item__description">{props.description}</p>
       </div>
     </li>
   );
--- resturant/src/Pages/MenuPage/MenuPage.jsx
+++ resturant/src/Pages/MenuPage/MenuPage.jsx
@@ -1,34 +1,53 @@
-import React from 'react'
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import MenuList from '../../Components/MenuList/MenuList';
 import Categories from '../../Components/Categories/Categories';
 
 function Menu() {
   const [menu, setMenu] = useState([]);
-const allCategories = ["all", "burgers","soups" , "drinks"];
-  
+  const [currentCategory, setCurrentCategory] = useState('all');
+  const allCategories = ['all', 'burgers', 'soups', 'drinks'];
+
   const url = 'http://localhost:8000';
+
   const getMenuData = async () => {
-    const response = await axios.get(`${url}/menu`);
-    console.log(response.data);
-    setMenu(response.data);
+    try {
+      const response = await axios.get(`${url}/menu`);
+      setMenu(response.data);
+    } catch (error) {
+      console.error('There was some error while fetching Menu data:', error);
+    }
   };
+
   useEffect(() => {
     getMenuData();
   }, []);
 
+  const filterItems = (category) => {
+    setCurrentCategory(category);
+  };
+
+  const filteredMenu =
+    currentCategory === 'all'
+      ? [...menu] 
+      : menu.filter(
+          (item) =>
+            item.category.toLowerCase() === currentCategory.toLowerCase()
+        );
+
   return (
     <main>
       <h1>Our Menu</h1>
-      <Categories categories={allCategories} />
+      <Categories categories={allCategories} filterItems={filterItems} />
 
       {!menu && <h1>Loading...</h1>}
       <ul>
-        {menu && menu.map((item) => <MenuList key={item.id} {...item} />)}
+        {filteredMenu.map((item) => (
+          <MenuList key={item.id} {...item} />
+        ))}
       </ul>
     </main>
   );
 }
 
-export default Menu
+export default Menu;
