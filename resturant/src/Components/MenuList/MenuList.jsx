7a2ab5bae3d96c4793090794569c15ae7ca56795
charuzain <charuabhizain@gmail.com>
2024-02-01 09:00:35 -0500

    Displayed MenuItems and Categories

--- resturant/src/Components/MenuList/MenuList.jsx
+++ resturant/src/Components/MenuList/MenuList.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
 const MenuList = (props) => {
+  console.log(props)
   return (
     <li>
       <img src={`http://localhost:8000${props.image}`} alt={props.name} />
+      <div className="item-info">
+        <header>
+          <h5>{props.title}</h5>
+          <span className="item-price">${props.price}</span>
+        </header>
+        <p className="item-text">{props.description}</p>
+      </div>
     </li>
   );
 }
