c0b208d5a73a1c408716b084b8cecebff6574610
Yashvimangroliya <yashvimangroliya@gmail.com>
2024-01-31 22:23:11 -0500

    header final commit

--- resturant/src/Components/Header/Header.js
+++ resturant/src/Components/Header/Header.js
@@ -1,31 +1,25 @@
-
-import { Link } from 'react-router-dom';
-import './Header.scss'; 
-import Logo from "../../assets/13.png"
+import { Link } from "react-router-dom";
+import "./Header.scss";
+import Logo from "../../assets/13.png";
 const Header = () => {
   return (
-   <nav>
-    <ul>
-       <Link>
-            <li className='header__logo-wrap'>
-                <img src={Logo} alt="logo" />
+    <nav className="nav">
+      <ul className="nav__list">
+        <div className="nav__logo-wrap">
+          <Link>
+            <li className="nav__logo">
+              <img src={Logo} alt="logo" />
             </li>
-       </Link>
-       <li className='header__home'>
-            Home
-       </li>
-       <li className='header__menu'>
-            Menu
-       </li>
-       <li className='header__about'>
-            About Us
-       </li>
-       <li className='header__facility'>
-            Facility
-       </li>
-       
-    </ul>
-   </nav>
+          </Link>
+        </div>
+        <div className="nav__items">
+          <li className="nav__home">Home</li>
+          <li className="nav__menu">Menu</li>
+          <li className="nav__about">About Us</li>
+          <li className="nav__facility">Facility</li>
+        </div>
+      </ul>
+    </nav>
   );
 };
 
