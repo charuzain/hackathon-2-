4fde9c29e3d392a8ab0ca4513bc68240c5da74ac
Yashvimangroliya <yashvimangroliya@gmail.com>
2024-02-01 13:01:25 -0500

    final collection commit

--- resturant/src/Components/Collection/Collection.js
+++ resturant/src/Components/Collection/Collection.js
@@ -1,20 +1,20 @@
 import "./Collection.scss";
-import burrito from "../../assets/7.png";
-import pettie from "../../assets/8.png";
+import burrito from "../../assets/6.png";
+import pettie from "../../assets/7.png";
 import salad from "../../assets/12.png";
 
 function Collection() {
   return (
-    <section>
-        <div>
-            <div>
-                <h2><span>Top </span>Collections</h2>
-                <p>All our meals in one delicious snap</p>
+    <section className="collection">
+        <div className="collection__wrap">
+            <div className="collection__title-wrap">
+                <h2 className="collection__title"><span className="collection__title-red">Top </span>Collections</h2>
+                <p className="collection__tag">All our meals in one delicious snap</p>
             </div>
-            <div>
-                <div><img src={burrito} alt="burrito" /></div>
-                <div><img src={pettie} alt="" /></div>
-                <div></div>
+            <div className="collection__imgs">
+                <div><img src={burrito} alt="burrito" className="collection__img" /></div>
+                <div><img src={pettie} alt="pettie" className="collection__img" /></div>
+                <div><img src={salad} alt="salad" className="collection__img" /></div>
             </div>
         </div>
     </section>
