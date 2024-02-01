4cda7f1b58c16fd673243673dc27405a81d6e2aa
charuzain <charuabhizain@gmail.com>
2024-02-01 10:42:49 -0500

    Add MenuDetailPage

---
+++ resturant/src/Pages/MenuDetailPage/MenuDetailPage.jsx
@@ -0,0 +1,14 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import './MenuDetailPage.scss';
+
+
+import React from 'react'
+
+const MenuDetailPage = () => {
+  return (
+    <div>MenuDetailPage</div>
+  )
+}
+
+export default MenuDetailPage
